5ed24bc2257fa35b692f0e6b647193548da3373d
Areeba Alam <areeba.25b01010090@abes.ac.in>
2026-07-23 15:20:38 +0530

    next commit

--- EventLoop/task1.js
+++ EventLoop/task1.js
@@ -1,18 +1,25 @@
 const f1 = () => {
-    console.log("f1");
+    console.log("f1 starts");
+    f2();
+    console.log("f1 running");
+    console.log("f1 end");
 };
 const f2 = () => {
-    console.log("f2");
+    console.log("f2 starts");
+    f3();
+    console.log("f2 running");
+    console.log("f2 end");
 };
 const f3 = () => {
-    console.log("f3");
+    console.log("f3 starts");
+    console.log("f3 running");
+    console.log("f3 stops");
 };
 function main()
 {
-    console.log("main");
+    console.log("main starts");
     f1();
-    f2();
-    f3();
-    console.log("end main");
+    console.log("main running");
+    console.log("main ends");
 }
 main();
